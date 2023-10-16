f1293125b2deac1495014552a1b897730e87e0b7
adfxxx <114661424+adfxxx@users.noreply.github.com>
2023-10-16 22:34:27 +0300

    Update task2.pl

--- task2.pl
+++ task2.pl
@@ -1,6 +1,30 @@
 % Task 2: Relational Data
 
-% The line below imports the data
-:- ['one.pl'].
+group(102,['Петров','Ивановский','Биткоинов','Шарпин','Эксель','Текстописов','Криптовалютников','Азурин','Круглотличников']).
+group(101,['Петровский','Сидоров','Мышин','Безумников','Густобуквенникова']).
+group(104,['Иванов','Запорожцев','Джаво','Фулл','Круглосчиталкин','Блокчейнис']).
+group(103,['Сидоркин','Эфиркина','Сиплюсплюсов','Программиро','Клавиатурникова','Решетников','Текстописова','Вебсервисов']).
+subject('Логическое программирование',[grade('Петров',4),grade('Петровский',2),grade('Иванов',5),grade('Ивановский',5),grade('Запорожцев',5),grade('Сидоров',2),grade('Сидоркин',4),grade('Биткоинов',4),grade('Эфиркина',3),grade('Сиплюсплюсов',4),grade('Программиро',4),grade('Джаво',2),grade('Клавиатурникова',5),grade('Мышин',3),grade('Фулл',5),grade('Безумников',5),grade('Шарпин',5),grade('Круглосчиталкин',4),grade('Решетников',5),grade('Эксель',4),grade('Текстописов',4),grade('Текстописова',5),grade('Густобуквенникова',5),grade('Криптовалютников',5),grade('Блокчейнис',3),grade('Азурин',5),grade('Вебсервисов',5),grade('Круглотличников',3)]).
+subject('Математический анализ',[grade('Петров',2),grade('Петровский',4),grade('Иванов',4),grade('Ивановский',4),grade('Запорожцев',4),grade('Сидоров',5),grade('Сидоркин',4),grade('Биткоинов',5),grade('Эфиркина',4),grade('Сиплюсплюсов',5),grade('Программиро',3),grade('Джаво',4),grade('Клавиатурникова',3),grade('Мышин',5),grade('Фулл',4),grade('Безумников',3),grade('Шарпин',4),grade('Круглосчиталкин',4),grade('Решетников',5),grade('Эксель',3),grade('Текстописов',4),grade('Текстописова',4),grade('Густобуквенникова',5),grade('Криптовалютников',4),grade('Блокчейнис',3),grade('Азурин',5),grade('Вебсервисов',4),grade('Круглотличников',5)]).
+subject('Функциональное программирование',[grade('Петров',3),grade('Петровский',5),grade('Иванов',4),grade('Ивановский',3),grade('Запорожцев',3),grade('Сидоров',3),grade('Сидоркин',4),grade('Биткоинов',5),grade('Эфиркина',5),grade('Сиплюсплюсов',5),grade('Программиро',4),grade('Джаво',5),grade('Клавиатурникова',5),grade('Мышин',5),grade('Фулл',4),grade('Безумников',5),grade('Шарпин',3),grade('Круглосчиталкин',3),grade('Решетников',4),grade('Эксель',4),grade('Текстописов',3),grade('Текстописова',4),grade('Густобуквенникова',4),grade('Криптовалютников',5),grade('Блокчейнис',5),grade('Азурин',4),grade('Вебсервисов',4),grade('Круглотличников',4)]).
+subject('Информатика',[grade('Петров',4),grade('Петровский',3),grade('Иванов',3),grade('Ивановский',4),grade('Запорожцев',3),grade('Сидоров',4),grade('Сидоркин',4),grade('Биткоинов',4),grade('Эфиркина',3),grade('Сиплюсплюсов',2),grade('Программиро',4),grade('Джаво',5),grade('Клавиатурникова',4),grade('Мышин',5),grade('Фулл',3),grade('Безумников',4),grade('Шарпин',5),grade('Круглосчиталкин',5),grade('Решетников',4),grade('Эксель',4),grade('Текстописов',3),grade('Текстописова',5),grade('Густобуквенникова',3),grade('Криптовалютников',2),grade('Блокчейнис',4),grade('Азурин',5),grade('Вебсервисов',4),grade('Круглотличников',4)]).
+subject('Английский язык',[grade('Петров',4),grade('Петровский',5),grade('Иванов',3),grade('Ивановский',4),grade('Запорожцев',4),grade('Сидоров',5),grade('Сидоркин',3),grade('Биткоинов',5),grade('Эфиркина',5),grade('Сиплюсплюсов',5),grade('Программиро',4),grade('Джаво',4),grade('Клавиатурникова',3),grade('Мышин',5),grade('Фулл',4),grade('Безумников',3),grade('Шарпин',4),grade('Круглосчиталкин',4),grade('Решетников',2),grade('Эксель',4),grade('Текстописов',4),grade('Текстописова',5),grade('Густобуквенникова',5),grade('Криптовалютников',5),grade('Блокчейнис',3),grade('Азурин',2),grade('Вебсервисов',4),grade('Круглотличников',4)]).
+subject('Психология',[grade('Петров',5),grade('Петровский',3),grade('Иванов',3),grade('Ивановский',3),grade('Запорожцев',5),grade('Сидоров',3),grade('Сидоркин',5),grade('Биткоинов',4),grade('Эфиркина',5),grade('Сиплюсплюсов',5),grade('Программиро',3),grade('Джаво',4),grade('Клавиатурникова',4),grade('Мышин',4),grade('Фулл',4),grade('Безумников',2),grade('Шарпин',3),grade('Круглосчиталкин',2),grade('Решетников',4),grade('Эксель',3),grade('Текстописов',3),grade('Текстописова',5),grade('Густобуквенникова',4),grade('Криптовалютников',5),grade('Блокчейнис',5),grade('Азурин',3),grade('Вебсервисов',5),grade('Круглотличников',4)]).
 
-group(X,L) :- findall(Z,student(X,Z),L).
+% Получить таблицу групп и средний балл по каждой из групп
+sum([], 0).
+sum([H|T], S) :- sum(T,S1), S is S1 + H.
+sr(L,Sr) :- length(L, Leng), sum(L,  Sum), Sr is Sum / Leng.
+grades(Student, N) :- subject(_,X), member(grade(Student, N), X).
+sr_student(Student, Sr) :- findall(X, grades(Student, X), List), sr(List, Sr).
+sr_student_group(Group, Sr) :- group(Group, Students), member(Student, Students), sr_student(Student, Sr).
+sr_groups :- group(Group, _), findall(X, sr_student_group(Group, X), List), sr(List, N), write('Group '), write(Group), write(', average grade: '), write(N), write('\n'), fail.
+
+% Для каждого предмета получить список студентов, не сдавших экзамен (grade=2)
+grade_2(Sub, Student) :- subject(Sub, Students), member(grade(Student, 2), Students).
+print_grade_2 :- subject(Sub, _), findall(Student, grade_2(Sub, Student), List), write('Subject: '), write(Sub), write(' Did not pass: '), write(List), write('\n'), fail.
+
+% Найти количество не сдавших студентов в каждой из групп
+grade_2_group(Group, Student) :- subject(_,X), group(Group,Y), member(Student,Y),member(grade(Student,2),X).
+sum_in_group(Group,Sum) :- findall(Student, grade_2_group(Group, Student), List), length(List, Sum).
+print_grade_2_group :- group(Group, _), sum_in_group(Group, Sum), write(Group), write(' Number:  '), write(Sum), write('\n'), fail.
